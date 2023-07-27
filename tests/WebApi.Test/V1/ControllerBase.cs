05475703a36f400db33db191fdf4e1afbeb4ed56
Jackson Costa <202203017276@alunos.estacio.br>
2023-07-27 16:37:56 -0300

    #14 Testes de unidade EndPoint

---
+++ tests/WebApi.Test/V1/ControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using MeuLivroDeReceitas.Exeptions;
+using System.Globalization;
+using Newtonsoft.Json;
+using Xunit;
+using System.Text;
+
+namespace WebApi.Test.V1;
+
+public class ControllerBase : IClassFixture<MeuLivroDeReceitaWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _httpClient;
+
+    public ControllerBase(MeuLivroDeReceitaWebApplicationFactory<Program> factory)
+    {
+        _httpClient = factory.CreateClient();
+        ResourceMensagensDeErro.Culture = CultureInfo.CurrentCulture;
+    }
+
+    protected async Task<HttpResponseMessage> PostRequest(string metodo, object body)
+    {
+        var jsonString = JsonConvert.SerializeObject(body);
+
+        return await _httpClient.PostAsync(metodo, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+    }
+}
---
+++ tests/WebApi.Test/V1/Usuario/Registrar/RegistrarUsuarioTeste.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using MeuLivroDeReceitas.Exeptions;
+using System.Net;
+using System.Text.Json;
+using Utilitario.Testes;
+using Xunit;
+
+namespace WebApi.Test.V1;
+
+public class RegistrarUsuarioteste : ControllerBase
+{
+    private const string METODO = "usuario";
+    public RegistrarUsuarioteste(MeuLivroDeReceitaWebApplicationFactory<Program> factory) 
+        : base(factory)
+    {
+
+    }
+
+    [Fact]
+    public async Task Validar_Sucesso()
+    {
+        var request = RequestRegistrarUsuarioBuilder.Contruir();
+
+        var resposta = await PostRequest(METODO, request);
+
+        resposta.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        await using var responseBody = await resposta.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Validar_Erro_Nome_Vazio()
+    {
+        var request = RequestRegistrarUsuarioBuilder.Contruir();
+        request.Nome= string.Empty;
+
+        var resposta = await PostRequest(METODO, request);
+
+        resposta.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await resposta.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var erro = responseData.RootElement.GetProperty("mensagens").EnumerateArray();
+        erro.Should().ContainSingle().And.Contain(c => c.GetString().Equals(ResourceMensagensDeErro.NOME_USUARIO_EMBRANCO));
+    }
+}
