19b5e2f4fe062d17542bbf26bc710b61da2c4cb0
Jackson Costa <202203017276@alunos.estacio.br>
2023-07-29 12:22:50 -0300

    #29 Refactor-Sonar-4

--- src/Backend/MeuLivroDeReceitas.Infrastructure/Database.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Database.cs
@@ -5,17 +5,32 @@ namespace MeuLivroDeReceitas.Infrastructure
 {
     public static class Database
     {
+        //public static void CriarDatabase(string connectionStrings, string nomeDatabase)
+        //{
+        //   using var minhaConexao = new MySqlConnection(connectionStrings);
+
+        //    var param = new DynamicParameters(nomeDatabase);
+        //    param.Add("nome", nomeDatabase);
+
+        //    var registros = minhaConexao.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @nome", param);
+
+        //    if (!registros.Any())
+        //        minhaConexao.Execute($"CREATE DATABASE {nomeDatabase}");
+        //}
         public static void CriarDatabase(string connectionStrings, string nomeDatabase)
         {
-           using var minhaConexao = new MySqlConnection(connectionStrings);
+            using var minhaConexao = new MySqlConnection(connectionStrings);
 
-            var param = new DynamicParameters(nomeDatabase);
+            var param = new DynamicParameters();
             param.Add("nome", nomeDatabase);
 
             var registros = minhaConexao.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @nome", param);
 
             if (!registros.Any())
-                minhaConexao.Execute($"CREATE DATABASE {nomeDatabase}");
+            {
+                // Utilizando parâmetros nomeados para evitar SQL injection
+                minhaConexao.Execute("CREATE DATABASE @nome", param);
+            }
         }
     }
 }
