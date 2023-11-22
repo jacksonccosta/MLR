a6ce34b7fc346d9d491b1cae7b92d652f2b38997
Jackson Costa <jackson.costa@uni9.edu.br>
2023-11-22 08:38:39 -0300

    #156 feat: Criação do Endpoiont Registrar Receita

---
+++ src/Backend/MeuLivroDeReceitas.Api/Controllers/ReceitaController.cs
@@ -0,0 +1,22 @@
+﻿using MeuLivroDeReceitas.Api.Filtros;
+using MeuLivroDeReceitas.Application;
+using MeuLivroDeReceitas.Comunicacao;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuLivroDeReceitas.Api.Controllers;
+
+[ServiceFilter(typeof(UsuarioAutenticadoAttribute))]
+public class ReceitaController : MlrController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseReceitaJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Registrar(
+        [FromServices] IRegistrarReceitaUseCase useCase, 
+        [FromBody] RequestRegistrarReceitaJson request)
+    {
+        var response = await useCase.Executar(request);
+
+        return Created(string.Empty, response);
+    }
+}
+
