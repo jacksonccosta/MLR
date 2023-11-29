0e2b6e2bd9821e4fe7fbdbda1859788834521a00
Jackson Costa <jackson.costa@uni9.edu.br>
2023-11-29 11:32:43 -0300

    #188 feat: Recuperar receita por id - implemento da regra de negócio

---
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Receita/RecuperarPorId/IRecuperarReceitaPorId.cs
@@ -0,0 +1,8 @@
+﻿using MeuLivroDeReceitas.Comunicacao;
+
+namespace MeuLivroDeReceitas.Application;
+
+public interface IRecuperarReceitaPorId
+{
+    Task<ResponseReceitaJson> Executar(long id);
+}
