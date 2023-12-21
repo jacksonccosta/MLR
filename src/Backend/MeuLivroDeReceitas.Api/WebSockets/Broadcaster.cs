eff531c6983f417659b18e8d747bbf9cb1b12dfd
Jackson Costa <jackson.costa@uni9.edu.br>
2023-12-21 10:03:15 -0300

    #219 Timer do QRCode com Broadcast

---
+++ src/Backend/MeuLivroDeReceitas.Api/WebSockets/Broadcaster.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace MeuLivroDeReceitas.Api.WebSockets;
+
+public class Broadcaster
+{
+    private readonly static Lazy<Broadcaster> _instance = new(() => new Broadcaster());
+    public static Broadcaster Instance {  get { return _instance.Value; } }
+    private ConcurrentDictionary<string, Conexao> _dictionary { get; set; }
+    public Broadcaster() 
+    {
+        _dictionary = new ConcurrentDictionary<string, Conexao>();
+    }
+
+    public void InicializarConexao(IHubContext<AdicionarConexao> hubContext, string connectionId)
+    {
+        var conexao = new Conexao(hubContext, connectionId);
+        _dictionary.TryAdd(connectionId, conexao);
+        conexao.IniciarContagemTempo(CallBackTempoExpirado);
+    }
+
+    private void CallBackTempoExpirado(string connectionId)
+    {
+        _dictionary.TryRemove(connectionId, out _);
+    }
+}
